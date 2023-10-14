a4ca11dd3e10e08b18f839834c14e7eb21e0476b
radhikaptil <patilradhika2001@gmail.com>
2023-10-14 20:36:11 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView,TemplateView,DetailView,CreateView
-
+from django.views.generic import ListView,TemplateView,DetailView,CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 from app.models import School
 
 class Home(TemplateView):
@@ -22,5 +22,16 @@ class SchoolDetail(DetailView):
 class SchoolCreate(CreateView):
     model=School
     fields='__all__'
+
+    
+class SchoolUpdate(UpdateView):
+    model=School
+    fields='__all__'
+
+
+class SchoolDelete(DeleteView):
+    model=School
+    context_object_name='schoolobject'
+    success_url=reverse_lazy('SchoolList')
 
     
